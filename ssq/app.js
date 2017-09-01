6a46e7945a614842555e64d2b074c5b5f914ede2
qinjun <qinjun@yrtong.com>
2017-09-01 18:34:40 +0800

    modified conflict before

--- ssq/app.js
+++ ssq/app.js
@@ -27,16 +27,11 @@ const cheerio = require('cheerio')
  */
 
 let ssq = { balls: [] }
-<<<<<<< HEAD
-const total = process.env.TOTAL || 108		// 截止到20170901，最后一期为2017102，总共105个页面记录
-=======
 const total = process.env.TOTAL || 108		// 截止到20170807，最后一期为2017091，总共108个页面记录
->>>>>>> 12254e3b9765926478527b5c846ffe4b5d4e8f40
 
 console.log(`开始获取页面数据，请耐心等待...`)
 getBalls(1)
 
-<<<<<<< HEAD
 function getBalls (num) {	// num为数字，表示第num页
   http.get('http://kaijiang.zhcw.com/zhcw/html/ssq/list_' + num + '.html', (res) => {
     let html = ''
@@ -85,59 +80,4 @@ function getBalls (num) {	// num为数字，表示第num页
     console.log(`> 获取第 ${num} 页数据失败~ (T_T)，重新获取中... error: ${e}`)
     getBalls(num)		// 获取失败时重新获取当页
   })
-=======
-function getBalls(num) {	// num为数字，表示第num页
-
-	http.get('http://kaijiang.zhcw.com/zhcw/html/ssq/list_' + num + '.html', (res) => {
-
-		let html = ''
-
-		res.on('data', (data) => { html += data })	// data为buffer数据
-
-		res.on('end', () => {
-
-			const $ = cheerio.load(html)		// 拿到的都是数字，不需要解决编码问题
-
-			if ($.html() === '') {
-				console.log(`第${num}页没有获取到数据~ (T_T)，重新获取中...`)
-				getBalls(num)
-				return
-			}
-
-			const $redBall = $('em.rr') 					// 拿到页面中所有红球
-			const $blueBall = $('em:not(.rr)')				// 拿到页面中所有蓝球
-			const $issueNumber = $('tr>td:nth-child(2)')	// 每一期的期号
-			const length = $blueBall.length 				// 页面期数以蓝球数量为准
-
-			let balls = []
-			let count = 0		// 用于记录红球
-			for (let i = 0; i < length; i++) {
-				let everyIssue = {}				// 记录每一期数据
-				everyIssue.issueNumber = $issueNumber[i].children[0].data 	// 当期期号
-				everyIssue.redBall = []			// 当期红球号码，以数组方式记录
-				for (let j = 0; j < 6; j++) {	// 每期6个红球
-					everyIssue.redBall.push($redBall[count].children[0].data)
-					count++
-				}
-				everyIssue.blueBall = $blueBall[i].children[0].data 		// 当期蓝球号码
-				balls.push(everyIssue)			// 将每期号码保存到数组中
-			}
-
-			ssq.balls = ssq.balls.concat(balls)			// 将当前页面记录的数据保存下来
-
-			console.log(`获取第 ${num} 页数据成功~ (^_^) ...`)
-			if (num == total) {
-				fs.writeFile(path.join(__dirname+'/data.json'), JSON.stringify(ssq))		// 保存为 json 格式
-				return
-			}
-
-			getBalls(num+1)    // 获取下一页数据
-		})
-
-	}).on('error', () => {
-		console.log(`获取第 ${num} 页数据失败~ (T_T)，重新获取中...`)
-		getBalls(num)		// 获取失败时重新获取当页
-	})
-
->>>>>>> 12254e3b9765926478527b5c846ffe4b5d4e8f40
 }
